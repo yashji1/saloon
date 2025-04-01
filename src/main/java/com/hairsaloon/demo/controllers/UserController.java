57e535189318115efe25561302872a9017567b39
yashji1 <sharmayashtdl@gmail.com>
2025-04-01 22:30:19 +0530

    all crud opertions on user

--- src/main/java/com/hairsaloon/demo/controllers/UserController.java
+++ src/main/java/com/hairsaloon/demo/controllers/UserController.java
@@ -3,12 +3,11 @@ package com.hairsaloon.demo.controllers;
 import com.hairsaloon.demo.modals.User;
 import com.hairsaloon.demo.repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -17,17 +16,46 @@ public class UserController {
     public userRepository userRepo;
     @PostMapping("/api/create-user")
     public User createUser(@RequestBody User user){
+        System.out.println("till here it is working ");
         return  userRepo.save(user);
     }
 
     @GetMapping("/api/user")
-    public User getUser(){
-        User user = new User();
-        user.setEmail("yashsharmaharsh@gmail.com");
-        user.setFullname("YASH SHARMA");
-        user.setRole("User");
-        user.setPhoneNo("+91 6395877792");
-        user.setCreatedAt(LocalDateTime.now());
-        return user;
+    public List<User> getUser(){
+        return userRepo.findAll();
     }
+    @GetMapping("/api/users/{id}")
+    public User getUserById(@PathVariable Long id) throws  Exception{
+        Optional<User>op = userRepo.findById(id);
+        if(op.isPresent())
+            return op.get();
+        throw  new Exception("USER NOT FOUND");
+    }
+    // Now we want to update user ;
+    @PutMapping("api/user/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Long id)throws Exception{
+        Optional<User> usr= userRepo.findById(id);
+
+        if(usr.isEmpty()){
+            throw new Exception("User not found with id "+ id);
+        }
+
+        User existingUser =  usr.get();
+
+        existingUser.setFullname(user.getFullname());
+        existingUser.setPhoneNo(user.getPhoneNo());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setRole(user.getRole());
+        return userRepo.save(existingUser);
+    }
+    @DeleteMapping("/api/user/remove/{id}")
+    public String removeUser(@PathVariable Long id) throws  Exception{
+        Optional<User> rmUser = userRepo.findById(id);
+        if(rmUser.isEmpty())
+            throw new Exception("User not present in the records with id"+id);
+        userRepo.deleteById(rmUser.get().getId());
+
+        return "User have been removed successfully";
+    }
+
 }
